7d4aa1d1b4b63aba69f2234f16f3f359a9d2e8c1
Leonid Semenov <kalesin90@gmail.com>
2018-10-11 18:03:52 +0300

    Add conected react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,28 @@
+import { connectRouter, routerMiddleware } from "connected-react-router";
+import { ConnectedRouter } from "connected-react-router";
+import { createBrowserHistory } from "history";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import App from "./App";
+import { Provider } from "react-redux";
+import { applyMiddleware, compose, createStore } from "redux";
+import rootReducer from "./reducers";
+import Routes from "./Routes";
 
-ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
+const history = createBrowserHistory();
+
+const composeEnhancer: typeof compose =
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  connectRouter(history)(rootReducer),
+  composeEnhancer(applyMiddleware(routerMiddleware(history)))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Routes />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById("root") as HTMLElement
+);
