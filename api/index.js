c9652ed5e84f549cf62d4ce428cd515d7fcf7a34
Leonid Semenov <kalesin90@gmail.com>
2018-11-14 22:21:02 +0300

    Add endpoints for orders

--- api/index.js
+++ api/index.js
@@ -1,12 +1,43 @@
 var express = require("express");
+var bodyParser = require('body-parser');
 var app = express();
 
-app.get("/", function(req, res) {
-  res.send("Hello World root!" + JSON.stringify(req.headers));
+const redis = require('redis');
+
+var host = 'redis';
+var port = 6379;
+
+var client = redis.createClient(port, host);
+
+client.on('error', (err) => {
+  console.log("Error " + err);
 });
 
-app.get("/hoho", function(req, res) {
-  res.send("Hello World hoho!");
+app.use(bodyParser.json())
+
+app.get("/", function(req, res) {
+  res.send("v1");
+});
+
+app.post("/orders", function(req, res) {
+  const {id} = req.body
+  console.log("id", id)
+  if (!id) return res.sendStatus(400)
+  
+  client.set(id, JSON.stringify(req.body), redis.print);
+
+  res.sendStatus(200)
+});
+
+app.get("/orders/:id", function(req, res) {
+  return client.get(req.params.id, (err, result) => {
+    if (result) {
+      const resultJSON = JSON.parse(result);
+      return res.status(200).json(resultJSON);
+    } else {
+      return res.sendStatus(404);
+    }
+  }) 
 });
 
 app.listen(8000, function() {
